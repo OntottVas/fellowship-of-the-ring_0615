1714e4b6e457c323623757f6679c221e862928b0
Geree <smithzero94@gmail.com>
2023-06-15 18:14:28 +0200

    elves and humans sets declared, and Legolas was added to the elves set, Aragorn and Boromir was added to the humans set.

--- src/FellowShipOfTheRing.java
+++ src/FellowShipOfTheRing.java
@@ -7,6 +7,14 @@ public class FellowShipOfTheRing {
         Set<String> dwarves = new HashSet<>();
         dwarves.add("Gimli");
 
+        Set<String> elves = new HashSet<>();
+        elves.add("Legolas");
+
+        Set<String> humans = new HashSet<>();
+        humans.add("Aragorn");
+        humans.add("Boromir");
+
+
 
     }
 }
