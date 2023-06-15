a9ec99389923914690db606b139b25dbbb1d323d
Geree <smithzero94@gmail.com>
2023-06-15 18:20:52 +0200

    hobbits set defined and added Frodo, Sam, Merry, Pippin to it with new methods as shown in the README.md file. Previous sets were filled with the method mentioned above. .add() method discarded.

--- src/FellowShipOfTheRing.java
+++ src/FellowShipOfTheRing.java
@@ -1,20 +1,18 @@
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class FellowShipOfTheRing {
     public static void main(String[] args) {
        // TODO
-        Set<String> dwarves = new HashSet<>();
-        dwarves.add("Gimli");
+        Set<String> dwarves = new HashSet<>(List.of("Gimli"));
 
-        Set<String> elves = new HashSet<>();
-        elves.add("Legolas");
+        Set<String> elves = new HashSet<>(List.of("Legolas"));
 
-        Set<String> humans = new HashSet<>();
-        humans.add("Aragorn");
-        humans.add("Boromir");
+        Set<String> humans = new HashSet<>(Arrays.asList("Aragorn", "Boromir"));
 
-
+        Set<String> hobbits = new HashSet<>(List.of("Frodo", "Sam", "Merry", "Pippin"));
 
     }
 }
