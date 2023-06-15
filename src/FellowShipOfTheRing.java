672d26392eda57abf8ef578573eb35031309d135
Geree <smithzero94@gmail.com>
2023-06-15 18:11:26 +0200

    dwarves set declared, Gimli was added to it.

--- src/FellowShipOfTheRing.java
+++ src/FellowShipOfTheRing.java
@@ -1,6 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class FellowShipOfTheRing {
     public static void main(String[] args) {
        // TODO
+        Set<String> dwarves = new HashSet<>();
+        dwarves.add("Gimli");
+
+
     }
 }
